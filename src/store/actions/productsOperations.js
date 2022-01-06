bdcb2b4f6c11bc9a7e7a453c1345bdbd14c2a2e3
Volodymyr Dvorskyi <feowurd@gmail.com>
2022-01-06 17:46:23 +0200

    changed way

--- src/store/actions/productsOperations.js
+++ src/store/actions/productsOperations.js
@@ -4,7 +4,7 @@ export const fetchProducts = createAsyncThunk(
   'FETCH_REQUEST_PRODUCT',
   async () => {
     const products = await fetch(
-      'http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline',
+      'https://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline',
     ).then(res => res.json());
     console.log(products);
     return products;
